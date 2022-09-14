2e375675c4baa7ea3aa525bd3df2f12fbc8d5e45
SireMiguel <root.serverfarm@gmail.com>
2022-09-14 23:58:36 +0200

    Fixed games/valorant

--- scripts/valorant.js
+++ scripts/valorant.js
@@ -1,20 +1,23 @@
 
 function randomMap () {
     const maps = ["Bind", "Haven", "Fracture", "Breeze", "Icebox", "Ascent", "Split", "Pearl"];
-    var map = maps[random(maps.length)];
-    console.log(map);   
+    const random = Math.floor(Math.random() * maps.length);
+    document.getElementById("text").innerHTML =
+        `${maps[random]}`;
 }
 
 function randomAgent () {
     const agents = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Jett", "Kay/O", "Killjoy", "Neon", "Omen",
     "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"];
-    var agent = agents[random(agents.length)];
-    console.log(agent);
+    const random = Math.floor(Math.random() * agents.length);
+    document.getElementById("text").innerHTML =
+        `${agents[random]}`;
 }
 
 function randomWeapon () {
     const weapons = ["Knife", "Classic", "Shorty", "Frenzy", "Ghost", "Sheriff", "Stinger", "Spectre" ,
     "Bucky", "Jugde", "Bulldog", "Guardian", "Phantom", "Vandal", "Marshal", "Operator", "Ares", "Odin"];
-    var weapon = weapons[random(weapons.length)];
-    console.log(weapon);
+    const random = Math.floor(Math.random() * weapons.length);
+    document.getElementById("text").innerHTML =
+        `${weapons[random]}`;
 }
