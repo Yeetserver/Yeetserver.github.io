5e02fcae5f81a462db8c891e540b54470915c400
SireMiguel <root.serverfarm@gmail.com>
2022-09-18 18:03:36 +0200

    Added random challenges

--- scripts/valorant.js
+++ scripts/valorant.js
@@ -26,3 +26,41 @@ function randomWeapon () {
     document.getElementById("displayText").innerHTML =
         `${sidearms[random_sidearm]} + ${weapons[random_weapon]}`;
 }
+
+var count = 3;
+
+function randomChallenge () {
+    var randomChallenges = [];
+    const challenges = ["Nur das Messer benutzen¹²", "Nur Zweitwaffen benutzen¹", "Nur Hauptwaffen benutzen¹", "Keine Waffen aufsammeln",
+    `Keine Waffen über ${randomInt(1, 50)*100} ¤ kaufen²`, `Keine Waffen unter ${randomInt(1, 50)*100} ¤ kaufen²`,
+    "Nur aufgesammelte Waffen benutzen²", "Nur Zweitwaffen aufsammeln²", "Nicht Springen", "Nur Schleichen⁴", "Keine Fähigkeiten einsetzen",
+    "Campen³", "Pushen³", "Nur Rückwärts laufen", "Nur Crouchen⁴", "Einen Gegner Messern¹²", "Nur Rennen⁴"];
+    function duplicateExists (symbol) {if (!randomChallenges.find(a =>a.includes(symbol)) + !randomChallenge.includes(symbol)) {return true} else {return false}}
+    for (let i = 1; i <= count; i++) {
+        var random = Math.floor(Math.random() * challenges.length);
+        var randomChallenge = challenges[random];
+        if (!randomChallenges.includes(randomChallenge) ) {
+            if (duplicateExists("¹") && duplicateExists("²") && duplicateExists("³") && duplicateExists("⁴")) {
+                randomChallenges.push(randomChallenge);
+            }
+            else {
+                i -= 1;
+            }
+        }
+        else {
+            i -= 1;
+        }
+    }
+
+    for(var i = 0; i < count; i++) {
+        var item = randomChallenges[i];
+        item = item.replace("¹", "");
+        item = item.replace("²", "");
+        item = item.replace("³", "");
+        item = item.replace("⁴", "");
+        randomChallenges[i] = item;
+    }
+
+    document.getElementById("displayText").innerHTML =
+        `${randomChallenges.join('<br>')}`;
+}
