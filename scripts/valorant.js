a26239486afee8445c62b1d388876bec6bdc1cc2
SireMiguel <root.serverfarm@gmail.com>
2022-09-27 23:33:58 +0200

    Added options

--- scripts/valorant.js
+++ scripts/valorant.js
@@ -1,8 +1,3 @@
-function changeImage(img) {
-    document.getElementById("img").src="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt26fcf1b5752514ee/5eb7cdbfc1dc88298d5d3799/V_AGENTS_587x900_Brimstone.png";
-}
-
-
 function randomMap () {
     const maps = ["Bind", "Haven", "Fracture", "Breeze", "Icebox", "Ascent", "Split", "Pearl"];
     const random = Math.floor(Math.random() * maps.length);
@@ -23,26 +18,56 @@ function randomWeapon () {
     const weapons = ["Stinger", "Spectre", "Bucky", "Jugde", "Bulldog", "Guardian", "Phantom", "Vandal", "Marshal", "Operator", "Ares", "Odin"];
     const random_sidearm = Math.floor(Math.random() * sidearms.length);
     const random_weapon = Math.floor(Math.random() * weapons.length);
-    document.getElementById("displayText").innerHTML =
+    if (getCookie("useSidearms") == "false" || getCookie("useSidearms") == "") {
+        document.getElementById("displayText").innerHTML =
         `${sidearms[random_sidearm]} + ${weapons[random_weapon]}`;
+    }
+    else {
+        document.getElementById("displayText").innerHTML =
+        `${weapons[random_weapon]}`;
+    }
+    
 }
 
-var count = 3;
-
 function randomChallenge () {
+
     var randomChallenges = [];
-    const challenges = ["Nur das Messer benutzen¹²", "Nur Zweitwaffen benutzen¹", "Nur Hauptwaffen benutzen¹", "Keine Waffen aufsammeln²⁶",
-    `Nur Waffen über ${randomInt(1, 40)*100} ¤ kaufen²`, `Nur Waffen unter ${randomInt(1, 40)*100} ¤ kaufen²`,
-    "Nur aufgesammelte Waffen benutzen²", "Nur Zweitwaffen aufsammeln²", "Nicht Springen", "Nur Schleichen⁴", "Keine Fähigkeiten einsetzen",
-    "Campen³", "Pushen³", "Nur Rückwärts laufen", "Nur Crouchen⁴", "Einen Gegner Messern¹²", "Nur Rennen⁴", "Nur Waffen vom Gegner benutzen¹²⁶",
-    "Nur auf die Füße zielen", "Einmal um die Map laufen³", "Nach A gehen³⁵", "Nach B gehen³⁵", "Nach C gehen³⁵"];
+    var difficultySymbol = "*"
+    var translatedDifficulty = "Einfach"
+
+    var difficulty = getCookie("difficulty");
+    var count = getCookie("count");
+
+    if (count === "") {count = 1};
+    if (difficulty === "") {difficulty = "easy"};
+
+    function convertDifficulty () {
+        if (difficulty == "easy") {difficultySymbol = "*"; translatedDifficulty = "Einfach";}
+        else if (difficulty == "medium") {difficultySymbol = "⁑"; translatedDifficulty = "Normal";}
+        else if (difficulty == "hard") {difficultySymbol = "⁂"; translatedDifficulty = "Schwer";}}
+    
+    convertDifficulty();
+
+    const challenges = ["Nur das Messer benutzen¹²⁂", "Nur Zweitwaffen benutzen¹*", "Nur Hauptwaffen benutzen¹*", "Keine Waffen aufsammeln²⁶*",
+    `Nur Waffen über ${randomInt(1, 40)*100} ¤ kaufen²⁑`, `Nur Waffen unter ${randomInt(1, 40)*100} ¤ kaufen²⁑`,
+    "Nur aufgesammelte Waffen benutzen²*", "Nur Zweitwaffen aufsammeln²*", "Nicht Springen*", "Nur Schleichen⁴*", "Keine Fähigkeiten einsetzen*",
+    "Campen³⁑", "Pushen³⁑", "Nur Rückwärts laufen⁂", "Nur Crouchen⁴⁑", "Einen Gegner Messern¹²⁑", "Nur Rennen⁴*", "Nur Waffen vom Gegner benutzen¹²⁶⁑",
+    "Nur auf die Füße zielen⁂", "Einmal um die Map laufen³⁂", "Nach A gehen³⁵*", "Nach B gehen³⁵*", "Nach C(A) gehen³⁵*", `Maximal ${randomInt(1, 5)} Gegner töten²⁑`];
+    
     function duplicateExists (symbol) {if (!randomChallenges.find(a =>a.includes(symbol)) + !randomChallenge.includes(symbol)) {return true} else {return false}}
+    if (challenges.length/2 < count) {count = Math.round(challenges.length/4)}
+
     for (let i = 1; i <= count; i++) {
         var random = Math.floor(Math.random() * challenges.length);
         var randomChallenge = challenges[random];
-        if (!randomChallenges.includes(randomChallenge) ) {
+        if (!randomChallenges.includes(randomChallenge)) {
             if (duplicateExists("¹") && duplicateExists("²") && duplicateExists("³") && duplicateExists("⁴") && duplicateExists("⁵") && duplicateExists("⁶")) {
-                randomChallenges.push(randomChallenge);
+                if (randomChallenge.includes(difficultySymbol)) {
+                    randomChallenges.push(randomChallenge);
+                }
+                else {
+                    i -= 1;
+                }
             }
             else {
                 i -= 1;
@@ -61,9 +86,40 @@ function randomChallenge () {
         item = item.replace("⁴", "");
         item = item.replace("⁵", "");
         item = item.replace("⁶", "");
+        item = item.replace("*", "");
+        item = item.replace("⁑", "");
+        item = item.replace("⁂", "");
         randomChallenges[i] = item;
     }
 
     document.getElementById("displayText").innerHTML =
         `${randomChallenges.join('<br>')}`;
+}
+
+function difficultySlider () {
+    var slider = document.getElementById("difficulty_slider");
+    var difficultyValue = document.getElementById("difficultyValue");
+    if (slider.value == 1) {document.cookie = "difficulty=easy"; var translatedDifficulty = "Einfach";}
+    else if (slider.value == 2) {document.cookie = "difficulty=medium"; var translatedDifficulty = "Normal";}
+    else if (slider.value == 3) {document.cookie = "difficulty=hard"; var translatedDifficulty = "Schwer";}
+    difficultyValue.innerHTML = `Schwierigkeit: ${translatedDifficulty}`
+}
+
+function countSlider () {
+    var slider = document.getElementById("count_slider");
+    var countValue = document.getElementById("countValue");
+    document.cookie = `count=${slider.value}`;
+    countValue.innerHTML = `Anzahl: ${getCookie("count")}`
+}
+
+function useSidearm () {
+    var useSidearm = document.getElementById("use_sidearms");
+    document.cookie = `useSidearms=${useSidearm.checked}`;
+    console.log(getCookie("useSidearms"));
+}
+
+function deleteValorantData () {
+    deleteCookie("count");
+    deleteCookie("difficulty");
+    deleteCookie("useSidearms");
 }
