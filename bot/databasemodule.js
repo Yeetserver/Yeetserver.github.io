84b7c52bde43db4028dd897f9955d9a32bebc2d3
SireMiguel <root.serverfarm@gmail.com>
2022-11-09 19:33:23 +0100

    QOL Image URL List

--- bot/databasemodule.js
+++ bot/databasemodule.js
@@ -27,6 +27,7 @@ export const auth = getAuth(app);
 const db = getDatabase();
 document.getElementById('send').addEventListener('click', send);
 document.getElementById('update').addEventListener('click', update);
+document.getElementById('showpfps').addEventListener('click', show_pfps);
 
 const reference = ref(db, 'dashboard');
 
@@ -35,13 +36,14 @@ function send() {
     var message = document.getElementById('message').value;
     var name = document.getElementById('name').value;
     var pfpUrl = document.getElementById('pfpUrl').value;
+    if (channel + message + name + pfpUrl == "") {return}
     set(ref(db, 'dashboard/message'), {
     channel: channel,
     pfpUrl: pfpUrl,
     name: name,
     message: message
 })
-console.log(`${name}: ${message}`)};
+console.log(`Nachricht erfolgreich gesendet.\nName: "${name}"\nProfilbild: "${pfpUrl}"\nChannel ID: "${channel}"\nNachricht: "${message}"`)};
 
 function update() {
     var status = document.getElementById('status').value;
@@ -51,14 +53,33 @@ function update() {
     type: type,
     status: status
 })
-console.log(`New status: ${status}, ${type}`)};
+console.log(`Neuer Status: ${status}\nTyp: ${type}`)};
+
+
+function show_pfps() {
+  const show_pfps_doc = document.getElementById('showpfps').value
+  const pfps_doc = document.getElementById('pfps').value
+  if (show_pfps_doc == "+") {
+    onValue(reference, (snapshot) => {
+      const raw_pfp_list = snapshot.val().pfps;
+      var pfp_list = ""
+      for (const object of raw_pfp_list) {
+        pfp_list = pfp_list.concat(`${object.name}:<br>${object.url}<br>`)
+      };
+      document.getElementById('pfps').innerHTML = `Liste an Profilbildern:<br>${pfp_list}`;
+      document.getElementById('showpfps').value = "-"
+    });
+  } else {
+    document.getElementById('pfps').innerHTML = "";
+    document.getElementById('showpfps').value = "+"
+  }
+}
 
 async function updateUptime(uptime) {
     document.getElementById('uptime').innerHTML = uptime;
 };
 
-const uptime = ref(db, 'dashboard');
-onValue(uptime, (snapshot) => {
+onValue(reference, (snapshot) => {
   const uptime = snapshot.val().uptime;
   if (uptime >= 3600000) {
     updateUptime(Math.round((uptime/1000/60/60)*10)/10 + " Stunden")
@@ -68,8 +89,8 @@ onValue(uptime, (snapshot) => {
   }//minute
 }); // FUNKTIONIERT NICHT RICHTIG PROP
 
-const enabledref = ref(db, 'dashboard');
-onValue(enabledref, (snapshot) => {
+
+onValue(reference, (snapshot) => {
   const enabled = snapshot.val().enabled;
   if (!enabled) {window.location.replace("https://yeetserver.github.io/settings/login")}
 });
