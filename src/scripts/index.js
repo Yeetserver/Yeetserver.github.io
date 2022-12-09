868440ff14b5050ae3248fcc30b07d32bec9ce8d
SireMiguel <root.serverfarm@gmail.com>
2022-12-09 19:40:26 +0100

    Removed the calculator

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,3 @@
-function calculator() {
-    var calculation = prompt("Rechnung eingeben")
-    try {
-        if (calculation.includes(",")) {
-            var calculation = calculation.replace(",", ".")
-        }
-        const result = eval(calculation)
-        if (result == undefined || result == null) {
-        } else {
-        if (Number.isInteger(result)) {
-            alert(`Ergebnis: ${result}`)
-        } else {
-            alert(`Genaues Ergebnis: ${result} \nGerundetes Ergebnis: ${Math.round(result)}`)
-        }}
-    }
-    catch {
-        alert(`Ungültige Rechnung: ${calculation}`)
-    }
-}
-
 function on() {
     document.getElementById("overlay").style.display = "block";
 }
