2d16770429de6e9b9c72ace8f634678798551718
SireMiguel <root.serverfarm@gmail.com>
2023-01-08 20:32:10 +0100

    Moved files

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-    function off() {
-    document.getElementById("overlay").style.display = "none";
-    document.cookie = "visited=true";
-}
